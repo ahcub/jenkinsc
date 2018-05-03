2d9c8fb3909e380ea970bc5c1108f38fdff0fd62
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2018-05-03 05:11:40 +0300

    !U update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name='jenkinsc',
     packages=find_packages(include=('jenkinsc', )),
-    version='0.0.22',
+    version='0.0.23',
     description='bulletproof jenkins client',
     author='Alex Buchkovsky',
     author_email='olex.buchkovsky@gmail.com',
