a38964d941d493cb0febd5df4aec71c988f59ae9
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2018-11-25 15:26:09 +0200

    !U fix the description source

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
-with open('README.txt') as file:
+with open('README.md') as file:
     long_description = file.read()
 
 
 setup(
     name='jenkinsc',
     packages=find_packages(include=('jenkinsc', )),
-    version='0.0.28',
+    version='0.0.30',
     description='bulletproof jenkins client',
     author='Alex Buchkovsky',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author_email='olex.buchkovsky@gmail.com',
     url='https://github.com/ahcub/jenkinsc',
     keywords=['jenkins', 'automation', 'ci', 'client', 'python'],
