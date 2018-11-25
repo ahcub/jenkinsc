a7a1974035dcb53102a7279f22ad9d82d5558952
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2018-11-25 15:06:33 +0200

    !U update the library description and add it to the pypi repository

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
+with open('README.md') as file:
+    long_description = file.read()
+
+
 setup(
     name='jenkinsc',
     packages=find_packages(include=('jenkinsc', )),
-    version='0.0.26',
+    version='0.0.27',
     description='bulletproof jenkins client',
     author='Alex Buchkovsky',
+    long_description=long_description,
     author_email='olex.buchkovsky@gmail.com',
     url='https://github.com/ahcub/jenkinsc',
     keywords=['jenkins', 'automation', 'ci', 'client', 'python'],
